4d5a2ba8ed87502bc09b098029244c1c36ac8958
Alena Shomanova <alenashomanova@gmail.com>
2023-04-18 16:46:15 -0500

    Fixes annotations for Allure report

--- src/test/java/tests/MainPageTests.java
+++ src/test/java/tests/MainPageTests.java
@@ -2,28 +2,30 @@ package tests;
 
 import io.qameta.allure.Owner;
 import io.qameta.allure.Story;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
 @Owner("Alena Shomanova")
+@Story("The main page's content is correct")
 @Tag("MainPageTests")
 public class MainPageTests extends TestBase {
 
-    @Story("The main page logo should be visible")
+    @DisplayName("The main page logo should be visible")
     @Test
     void logoExistsTest() {
         mainPage.acceptCookies()
                 .checkLogoVisible();
     }
 
-    @Story("There are 7 services listed in the OUR SERVICES section")
+    @DisplayName("There are 7 services listed in the OUR SERVICES section")
     @Test
     void ourServicesAmountTest() {
         mainPage.acceptCookies()
                 .verifyOurServicesAmount();
     }
 
-    @Story("The footer has a link to the 4 social networks")
+    @DisplayName("The footer has a link to the 4 social networks")
     @Test
     void socialNetworkAmountTest() {
         mainPage.acceptCookies()
--- src/test/java/tests/SearchTests.java
+++ src/test/java/tests/SearchTests.java
@@ -2,21 +2,23 @@ package tests;
 
 import io.qameta.allure.Owner;
 import io.qameta.allure.Story;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
 @Owner("Alena Shomanova")
+@Story("Make a search with a popular keyword")
 @Tag("SearchTests")
 public class SearchTests extends TestBase {
-    @Story("The search header displays the correct text")
+    @DisplayName("The search header displays the correct text")
     @Test
-    void logoExistsTest() {
+    void searchHeaderCorrectText() {
         String searchText = "QA";
         mainPage.initiateSearchWithText(searchText);
         searchPage.verifyHeaderText(searchText);
     }
 
-    @Story("The search results start from the first page")
+    @DisplayName("The search results start from the first page")
     @Test
     void searchStartsWithFirstPageTest() {
         String searchText = "QA";
@@ -24,7 +26,7 @@ public class SearchTests extends TestBase {
         searchPage.verifyPageNumberOne();
     }
 
-    @Story("The search first page has 10 items")
+    @DisplayName("The search first page has 10 items")
     @Test
     void searchFirstPageAmountTest() {
         String searchText = "QA";
