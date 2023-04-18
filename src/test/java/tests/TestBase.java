cc98f2f9a311e184c9fb59cd09111a55e956a361
Alena Shomanova <alenashomanova@gmail.com>
2023-04-18 15:43:37 -0500

    Added Allure Notifications plugin

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -1,4 +1,5 @@
 package tests;
+import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import helpers.Attach;
@@ -7,18 +8,40 @@ import io.qameta.allure.selenide.AllureSelenide;
 import objects.MainPage;
 import objects.SearchPage;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.remote.DesiredCapabilities;
 
+import java.util.Map;
+
+import static com.codeborne.selenide.Configuration.baseUrl;
 import static com.codeborne.selenide.Selenide.closeWebDriver;
 
 public class TestBase {
     MainPage mainPage = new MainPage();
     SearchPage searchPage = new SearchPage();
 
+    @BeforeAll
+    static void beforeAll() {
+        baseUrl = "https://www.luxoft.com/";
+        Configuration.browser =  System.getProperty("browser", "chrome");
+        Configuration.browserVersion = System.getProperty("browser_version", "100.0");
+        Configuration.browserSize = "1920x1080";
+        Configuration.remote = System.getProperty("remoteUrl", "https://user1:1234@selenoid.autotests.cloud/wd/hub");
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("selenoid:options", Map.<String, Object>of(
+                "enableVNC", true,
+                "enableVideo", true
+        ));
+
+        Configuration.browserCapabilities = capabilities;
+    }
+
     @BeforeEach
     @Story("Open the main page")
     void addListener() {
-        Selenide.open("https://www.luxoft.com/");
+        Selenide.open(baseUrl);
         SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
     }
     @AfterEach
