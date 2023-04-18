fca5a6009ba25736d0b1d6036e860aa4983d5507
Alena Shomanova <alenashomanova@gmail.com>
2023-04-18 16:25:48 -0500

    Removes unused parameters

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -23,11 +23,10 @@ public class TestBase {
 
     @BeforeAll
     static void beforeAll() {
-        baseUrl = "https://www.luxoft.com/";
-        Configuration.browser =  System.getProperty("browser", "chrome");
-        Configuration.browserVersion = System.getProperty("browser_version", "100.0");
+        Configuration.baseUrl = "https://www.luxoft.com/";
+        Configuration.browser = "chrome";
         Configuration.browserSize = "1920x1080";
-        Configuration.remote = System.getProperty("remoteUrl", "https://user1:1234@selenoid.autotests.cloud/wd/hub");
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
 
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("selenoid:options", Map.<String, Object>of(
