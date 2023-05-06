84a9f56b30f03022137e85c2c9b8f3d390413bd0
Alena Shomanova <alenashomanova@gmail.com>
2023-05-06 18:03:22 -0500

    Hides auth data

--- src/test/java/config/ProjectConfiguration.java
+++ src/test/java/config/ProjectConfiguration.java
@@ -8,13 +8,15 @@ import java.util.Map;
 
 public class ProjectConfiguration {
     static WebConfig webConfig = ConfigFactory.create(WebConfig.class, System.getProperties());
+    static AuthConfig authconfig = ConfigFactory.create(AuthConfig.class, System.getProperties());
 
     public void configure() {
         Configuration.baseUrl = webConfig.baseUrl();
         Configuration.browser = webConfig.browser();
         Configuration.browserVersion = webConfig.browserVersion();
         Configuration.browserSize = webConfig.browserSize();
-        String remoteUrl = webConfig.remoteUrl();
+        String remoteUrl = "https://" + authconfig.remote_username() + ":" + authconfig.remote_password() + "@" + webConfig.remoteUrl()  + "/wd/hub";
+        System.out.println(remoteUrl);
         if (webConfig.isRemote()) {
             Configuration.remote = remoteUrl;
             DesiredCapabilities capabilities = new DesiredCapabilities();
